1f8825d4565544d9a22eead8c9a7759a0eed1e9e
Matthew Coon <66556546+mattcoon@users.noreply.github.com>
2025-06-28 07:58:49 -0400

    set z_max to 100mm for LR4 and default to 0 when unknown

--- Marlin/src/module/motion.cpp
+++ Marlin/src/module/motion.cpp
@@ -94,7 +94,7 @@ bool relative_mode; // = false
 #ifdef Z_IDLE_HEIGHT
   #define Z_INIT_POS Z_IDLE_HEIGHT
 #else
-  #define Z_INIT_POS Z_HOME_POS
+  #define Z_INIT_POS Z_MIN_POS
 #endif
 
 xyze_pos_t current_position = LOGICAL_AXIS_ARRAY(0, X_HOME_POS, Y_HOME_POS, Z_INIT_POS, I_HOME_POS, J_HOME_POS, K_HOME_POS, U_HOME_POS, V_HOME_POS, W_HOME_POS);
